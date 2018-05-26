f949c05a2a6f1ebaadda354f9045947f65ca018a
Daniil Bondarau <Slayerdog4@gmail.com>
2018-05-26 08:38:05 +0300

    updated UI

--- UI/main.js
+++ UI/main.js
@@ -1,33 +1,70 @@
-//==========================MODAL BOX===================================
 'use strict';
-(function() {
-    var audio = new Audio('res/DIMOON.mp3');
-    function handleInsta() {
-        audio.volume = 0.04;
-        audio.play();
-        document.body.style.background = 'url(res/DANK_GIF.gif)';
+
+let users = [
+    {
+        username: 'admin',
+        password: '123123123',
+        liked: []
+    },
+    {
+        username: 'Hlib',
+        password: '123',
+        liked: []
+    },
+    {
+        username: 'Trururump',
+        password: "abacaba",
+        liked: []
+    },
+    {
+        username: 'aspelled',
+        password: 'myxtap',
+        liked: []
+    },
+    {
+        username: 'Nova',
+        password: 'lolkekmlg',
+        liked: []
+    },
+    {
+        username: 'Kiddy',
+        password: 'qwerty',
+        liked: []
     }
-    function undoHandleInsta() {
-        document.body.style.background = '';
-        audio.pause();
-    }
-    var insta = document.querySelector('.instagram');
-    insta.addEventListener('mouseover', handleInsta);
-    insta.addEventListener('mouseout', undoHandleInsta);
-}());
+];
+
+let currentUser = {
+    username: 'guest',
+    password: ''
+};
+
+function validateUser() {
+    let username = document.querySelector('.username').value;
+    let password = document.querySelector('.password').value;
+
+    username = username || null;
+    password = password || null;
+    
+    users.forEach((element) => {
+        if (username === element.username && password === element.password) {
+            currentUser.username = username;
+            currentUser.password = password;
+        }
+    });
+    document.querySelector('.wrapper').style.display = 'none';
+    document.querySelector('.user-info').style.display = 'flex';
+    console.log(currentUser);
+}
+
+function logout() {
+    currentUser.username = 'guest';
+    currentUser.password = '';
+    document.querySelector('.wrapper').style.display = 'flex';
+    document.querySelector('.user-info').style.display = 'none';
+}
+
 (function(){
-    var modal = document.getElementById('myModal');
-    console.log(modal);
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    /*var artile = {
-        img: [ {
-                imgSrc: ,
-                description: ,
-                valueOfLucas: ,
-            }
-        ]
-    };*/
+
     var articles = [
         {
             id: '1',
@@ -70,18 +107,22 @@
             nickName: 'Kiddy',
             createdAt: new Date('2018-02-23T23:00:00'),
             imgSrc: 'res/YKiviMHC8NI.jpg'
+        },
+        {
+            id: '7',
+            description: 'FAMCS IS DA BEST! BUT U NOU DE WEY?..',
+            nickName: 'Kiddy',
+            createdAt: new Date('2018-02-23T23:00:00'),
+            imgSrc: 'res/YKiviMHC8NI.jpg'
+        },
+        {
+            id: '8',
+            description: 'FAMCS IS DA BEST! BUT U NOU DE WEY?..',
+            nickName: 'Kiddy',
+            createdAt: new Date('2018-02-23T23:00:00'),
+            imgSrc: 'res/YKiviMHC8NI.jpg'
         }
     ];
-
-
-    function getPhotoPosts(skip, top, filterConfig) {
-        if (skip < 0 || top < 0) {
-            return undefined;
-        }
-        var posts = takePhotoPosts();
-        skip = skip || 0;
-        top = top || 0;
-    }
     
     function takePhotoPosts() {
         var posts = JSON.parse(localStorage.getItem('posts'));
@@ -90,37 +131,80 @@
         });
         return posts;
     }
-    
-    document.getElementById('myImg').addEventListener('click', aa);
-    /*artile.img.forEach(item, i => {
 
+    articles.forEach(element => {
+        appendPost(element);
     });
-    function createArtile( ){
 
-    }*/
-    function aa(e) {
-        modal.style.display = "block";
-        var nickName = document.createTextNode(this.alt);
-        nickName.style.color = "#0ff";
-        captionText.appendChild(nickName);
-        modalImg.src = this.src;
-        captionText.style.font = "italic bold 20px arial";
-        captionText.style.color = "#000";
-        var lukas = new Image();
-        lukas.src = 'res/Like.png';
-        lukas.id = "like-btn";
-    }
-
-
-
-// Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
 }());
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 
-//==========================LIKE BUTTON=================================
+function tempor() {
+    let modals = document.querySelectorAll('.modal');
+    let imgs = document.querySelectorAll('.dank');
+    let modalImgs = document.querySelectorAll('.modal-content');
+    let spans = document.querySelectorAll('.close');
+
+    for (let i = 0; i < modals.length; i++) {
+        imgs[i].onclick = function () {
+            modals[i].style.display = 'block';
+            modalImgs[i].src = this.src;
+        };
+        spans[i].onclick = function () {
+            modals[i].style.display = 'none';
+        };
+    }
+
+}
+
+let modal = document.querySelector('.modal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.querySelector('.dank');
+var modalImg = document.querySelector(".modal-content");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+};
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+function like() {
+    if (currentUser.username === 'guest') {
+        console.log('nope')
+    } else {
+        document.querySelector()
+    }
+}
+
+function createPost(post) {
+    let postTemplate = document.createElement('div');
+    postTemplate.innerHTML = `
+            <div class="card-content1">
+                <img class="dank" id="myImg" src="${post.imgSrc}" alt="${post.nickName}">
+                <div id="myModal" class="modal">
+                    <span class="close">&times;</span>
+                    <img src="" class="modal-content" id="img01">
+                    <div class="caption" style="background-color: white">
+                        <img class="like-btn" src="res/Like.png" onclick="like()"/>
+                        <p class="img-description">${post.description}</p>
+                        <p class="nickname">${post.nickName}</p>
+                        <div class="upload-date">${post.createdAt}</div>
+                    </div>
+                </div>
+            </div>
+        `;
+    return postTemplate.firstElementChild;
+}
+
+function appendPost(post) {
+    let temp = createPost(post);
+    document.querySelector('.card').appendChild(temp);
+    tempor();
+}
