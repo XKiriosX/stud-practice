70046ab286b37f51eb162eee43ffac0a0b05b2f5
Daniil Bondarau <Slayerdog4@gmail.com>
2018-05-29 08:35:24 +0300

    initial commit on lab11

--- UI/main.js
+++ UI/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 let users = [
     {
         username: 'admin',
@@ -39,11 +37,14 @@ let currentUser = {
 };
 
 function validateUser() {
-    let username = document.querySelector('.username').value;
+    let username = document.querySelector('.username-input').value;
     let password = document.querySelector('.password').value;
+    console.log(username, password);
 
-    username = username || null;
-    password = password || null;
+    if (username === undefined && password === '') {
+        alert('Input required');
+        return;
+    }
     
     users.forEach((element) => {
         if (username === element.username && password === element.password) {
@@ -51,6 +52,10 @@ function validateUser() {
             currentUser.password = password;
         }
     });
+    if (currentUser.username === 'guest' && currentUser.password === '') {
+        alert('User not found');
+        return;
+    }
     document.querySelector('.wrapper').style.display = 'none';
     document.querySelector('.user-info').style.display = 'flex';
     console.log(currentUser);
@@ -71,133 +76,131 @@ function logout() {
             description: 'OMAGAD WTF BBQ?!?!?!',
             nickName: 'Hlib',
             createdAt: new Date('2018-02-23T23:00:00'),
-            imgSrc: 'res/Hlib-Paper.jpg'
+            imgSrc: 'res/Hlib-Paper.jpg',
+            likedBy: []
         },
         {
             id: '2',
             description: 'Da best Wild West cowboys',
             nickName: 'Nova',
             createdAt: new Date('2018-02-23T23:00:00'),
-            imgSrc: 'res/filmy-pro-kovboev_jpg_770x430_crop_q70.jpg'
+            imgSrc: 'res/filmy-pro-kovboev_jpg_770x430_crop_q70.jpg',
+            likedBy: []
         },
         {
             id: '3',
             description: 'May our best pilots win dis war!',
             nickName: 'aspelled',
             createdAt: new Date('2018-02-23T23:00:00'),
-            imgSrc: 'res/Nikita-Max.jpg'
+            imgSrc: 'res/Nikita-Max.jpg',
+            likedBy: []
         },
         {
             id: '4',
             description: 'Landscapes of America N1',
             nickName: 'Trururump',
             createdAt: new Date('2018-02-23T23:00:00'),
-            imgSrc: 'res/quebrada-de-umahuaca.jpg'
+            imgSrc: 'res/quebrada-de-umahuaca.jpg',
+            likedBy: []
         },
         {
             id: '5',
             description: 'Landscapes of America N2',
             nickName: 'Trururump',
             createdAt: new Date('2018-02-23T23:00:00'),
-            imgSrc: 'res/surreal-landscapes-cristales.jpg'
+            imgSrc: 'res/surreal-landscapes-cristales.jpg',
+            likedBy: []
         },
         {
             id: '6',
             description: 'FAMCS IS DA BEST! BUT U NOU DE WEY?..',
             nickName: 'Kiddy',
             createdAt: new Date('2018-02-23T23:00:00'),
-            imgSrc: 'res/YKiviMHC8NI.jpg'
+            imgSrc: 'res/YKiviMHC8NI.jpg',
+            likedBy: []
         },
         {
             id: '7',
             description: 'FAMCS IS DA BEST! BUT U NOU DE WEY?..',
             nickName: 'Kiddy',
             createdAt: new Date('2018-02-23T23:00:00'),
-            imgSrc: 'res/YKiviMHC8NI.jpg'
+            imgSrc: 'res/YKiviMHC8NI.jpg',
+            likedBy: []
         },
         {
             id: '8',
             description: 'FAMCS IS DA BEST! BUT U NOU DE WEY?..',
             nickName: 'Kiddy',
             createdAt: new Date('2018-02-23T23:00:00'),
-            imgSrc: 'res/YKiviMHC8NI.jpg'
+            imgSrc: 'res/YKiviMHC8NI.jpg',
+            likedBy: []
         }
     ];
-    
-    function takePhotoPosts() {
-        var posts = JSON.parse(localStorage.getItem('posts'));
-        posts.forEach(function (value) {
-            value.createdAt = new Date(value.createdAt);
-        });
-        return posts;
+
+    function getPhotoPost(id) {
+        return articles.find(el => el.id === id);
     }
 
     articles.forEach(element => {
         appendPost(element);
     });
 
+    document.addEventListener('click', eventProcessing, false);
+
+    function eventProcessing(event) {
+        const ESC_KEYCODE = 27;
+        let current = event.target.closest('.card-content1');
+        let currentId = current.id;
+        if (event.target.className === 'dank') {
+            let res = getPhotoPost(currentId);
+            let holder = document.createElement('div');
+            holder.innerHTML = `
+                <div class="modal" style="display: block">
+                    <span class="close">&times;</span>
+                    <img src="${res.imgSrc}" class="modal-content">
+                    <div class="caption" style="background-color: white">
+                        <img class="like-btn" src="res/Like.png"/>
+                        <i class="edit-btn far fa-edit"></i>
+                        <p class="img-description">${res.description}</p>
+                        <p class="nickname">${res.nickName}</p>
+                        <div class="upload-date">${res.createdAt.getDay()}-${res.createdAt.getMonth()}-${res.createdAt.getFullYear()}</div>
+                    </div>
+                </div>
+            `;
+            current.appendChild(holder.firstElementChild);
+            window.addEventListener('keydown', closeModalByEsc, false);
+        }
+        if (event.target.className === "close") {
+            current.removeChild(event.target.closest('.modal'));
+        }
+        if (event.target.className === 'like-btn') {
+            if (currentUser.username === 'guest') {
+                alert('U must be logged in to like posts');
+            } else {
+                event.target.src = './res/Liked.png';
+                articles.forEach(el => {
+                    if (el.id === currentId) {
+                        el.likedBy.push(currentUser.username);
+                    }
+                });
+            }
+        }
+        function closeModalByEsc(ev) {
+            if (ev.keyCode === ESC_KEYCODE) {
+                window.removeEventListener('keydown', closeModalByEsc);
+                current.removeChild(document.querySelector('.modal'));
+            }
+        }
+    }
+
 }());
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-
-function tempor() {
-    let modals = document.querySelectorAll('.modal');
-    let imgs = document.querySelectorAll('.dank');
-    let modalImgs = document.querySelectorAll('.modal-content');
-    let spans = document.querySelectorAll('.close');
-
-    for (let i = 0; i < modals.length; i++) {
-        imgs[i].onclick = function () {
-            modals[i].style.display = 'block';
-            modalImgs[i].src = this.src;
-        };
-        spans[i].onclick = function () {
-            modals[i].style.display = 'none';
-        };
-    }
-
-}
-
-let modal = document.querySelector('.modal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.querySelector('.dank');
-var modalImg = document.querySelector(".modal-content");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-};
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-};
-
-function like() {
-    if (currentUser.username === 'guest') {
-        console.log('nope')
-    } else {
-        document.querySelector()
-    }
-}
 
 function createPost(post) {
     let postTemplate = document.createElement('div');
     postTemplate.innerHTML = `
-            <div class="card-content1">
-                <img class="dank" id="myImg" src="${post.imgSrc}" alt="${post.nickName}">
-                <div id="myModal" class="modal">
-                    <span class="close">&times;</span>
-                    <img src="" class="modal-content" id="img01">
-                    <div class="caption" style="background-color: white">
-                        <img class="like-btn" src="res/Like.png" onclick="like()"/>
-                        <p class="img-description">${post.description}</p>
-                        <p class="nickname">${post.nickName}</p>
-                        <div class="upload-date">${post.createdAt}</div>
-                    </div>
-                </div>
+            <div class="card-content1" id="${post.id}">
+                <img class="dank" src="${post.imgSrc}" alt="${post.nickName}">
+                
             </div>
         `;
     return postTemplate.firstElementChild;
@@ -206,5 +209,4 @@ function createPost(post) {
 function appendPost(post) {
     let temp = createPost(post);
     document.querySelector('.card').appendChild(temp);
-    tempor();
 }
